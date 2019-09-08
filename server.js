235b207583921f40b1e4809d35c18e0dd62951eb
sallysy8 <sallysy8@hotmail.com>
2019-09-08 11:27:33 -0700

    Add subrouters for users

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //DB config
@@ -10,5 +14,10 @@ mongoose.connect(db).then(() => console.log('MongoDB connected')).catch(err => c
 
 //First Route
 app.get('/', (req,res) => res.send('Hello!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = 7000;
 app.listen(port, ()=> console.log(`Server running on port ${port} `));
