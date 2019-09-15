5a29b3316207baa270d92c45d8b10bf0be21a8fe
sallysy8 <sallysy8@hotmail.com>
2019-09-15 14:00:09 -0700

    Complete register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @route  POST api/users/register
 // @description Register a user
@@ -12,14 +14,47 @@ router.post("/register", (req, res) => {
 			if (user) {
 				return res.status(400).json({ email: "Email already exists" });
 			} else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm',
+                });
 				const newUser = new User({
 					name: req.body.name,
-					eamil: req.body.email,
+                    email: req.body.email,
+                    avatar,
 					password: req.body.password
-				});
+                });
+                
+                bcrypt.genSalt(10, (err,salt) => {
+                    if (err) throw err;
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save().then(user => res.json(user)).catch(err =>console.log(err));
+                    })
+                })
 			}
 		})
 		.catch(err => console.log(err));
 });
+
+// @route  POST api/users/login
+// @description Login a user / return JWT token
+// @access Public
+router.post("/login", (req, res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+ 
+    //Find user by email (email = email)
+    User.findOne({email}).then(user =>{
+        if (!user){
+            return res.status(400).json({
+                email: 'User not found'
+            });
+        }
+    }).catch(err => console.log(err));
+});
+
 
 module.exports = router;
