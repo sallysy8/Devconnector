7724e6b2ac700c7b012e41187ebcc55c66336e7e
sallysy8 <sallysy8@hotmail.com>
2019-10-13 11:25:22 -0700

    Add Redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
+import { registerUser } from '../../actions/authActions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Register extends Component {
     constructor(){
@@ -30,11 +33,14 @@ class Register extends Component {
             password2: this.state.password2
         };
 
-        axios.post('/api/users/register', newUser).then(res => console.log(res.data))
-        .catch(err => this.setState({
-            errors: err.response.data
-        }));
+        this.props.registerUser(newUser, this.props.history);
 
+    }
+
+    componentWillReceiveProps(nextProps){
+       if(nextProps.errors){
+         this.setState({errors: nextProps.errors})
+       };
     }
 
     render() {
@@ -80,4 +86,15 @@ class Register extends Component {
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps, {registerUser})( Register);
